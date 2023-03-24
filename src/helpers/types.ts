6041f2095e7f8db951c44e1a527d3b9d38b82c9a
Андрей Епифанов <epifanovmd@gmail.com>
2023-03-24 12:23:01 +0300

    add types

--- src/helpers/types.ts
+++ src/helpers/types.ts
@@ -38,6 +38,6 @@ export interface IEmpty {
 export type Maybe<T> = T | undefined;
 
 export interface SupportInitialize<T = never> {
-  initialize(props?: T): undefined | (() => void);
-  onFocusScreen(isFocused: boolean): void;
+  initialize(props: T): undefined | (() => void);
+  onFocusScreen(isFocused: boolean, props: T): void;
 }
