d06e2a24b148d384182af52dfe23b3416498fc3b
Андрей Епифанов <epifanovmd@gmail.com>
2023-10-27 09:34:29 +0300

    fix export

--- src/store/holders/index.ts
+++ src/store/holders/index.ts
@@ -5,5 +5,6 @@ export * from "./FormHolder";
 export * from "./ListCollectionHolder";
 export * from "./PropsHolder";
 export * from "./TextHolder";
+export * from "./NumberHolder";
 export * from "./validations";
 export * from "./ValueHolder";
