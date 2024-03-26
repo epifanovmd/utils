d943fa5e0b83944b8aab26296c3417798ac42577
Андрей Епифанов <epifanovmd@gmail.com>
2024-03-26 13:26:47 +0300

    refactoring

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -2,6 +2,7 @@ export * from "./array";
 export * from "./debounce";
 export * from "./delayPromise";
 export * from "./disposer.helper";
+export * from "./id.helper";
 export * from "./input";
 export * from "./initialize.helper";
 export * from "./lambdaValue";
