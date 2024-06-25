f3ab94c0c0263a98d3cfef3ba2da81a5fe5ffd35
Андрей Епифанов <epifanovmd@gmail.com>
2024-06-25 10:32:12 +0300

    add promisify

--- src/helpers/promise.ts
+++ src/helpers/promise.ts
@@ -2,3 +2,11 @@ export const isPromise = <T>(obj: any | Promise<T>): obj is Promise<T> =>
   !!obj &&
   (typeof obj === "object" || typeof obj === "function") &&
   typeof obj.then === "function";
+
+export const promisify = <P>(value: P) => {
+  if (isPromise(value)) {
+    return value as P;
+  }
+
+  return Promise.resolve<P>(value);
+};
